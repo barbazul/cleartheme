ed0a0835311de4e44b87bfc52fb110c7642eebe4
Emiliano Mateu <mateuemiliano@gmail.com>
2013-03-05 19:58:31 -0300

    Cart page cleanup and general css improvements.
    CSS cleanup and improvements on Cart page.
    Minor css adjustments in page/form.less.
    Cleaned/moved lot of css from page/page.less.
    Created catalog/general.less to handle common catalog styles.
    Created checkout/general.less to handle common checkout styles.
    
    Local.xml
    - Added css/catalog/general.less in following handlers:
    --- <catalog_product_view>
    --- <review_product_list>
    --- <review_product_view>
    --- <catalog_product_compare_index>
    --- <catalog_category_default>
    --- <catalog_category_layered>
    --- <catalogsearch_result_index>
    --- <catalogsearch_advanced_result>
    --- <tag_product_list>
    --- <checkout_cart_index>
    --- <checkout_onepage_index>
    --- <customer_account>
    
    Added css/checkout/general.less in following handlers:
    --- <checkout_cart_index>
    --- <checkout_onepage_index>

--- src/skin/frontend/cleartheme/default/js/cart.js
+++ src/skin/frontend/cleartheme/default/js/cart.js
@@ -4,10 +4,22 @@
  *
  */
 
-//(function ($) {
-//    $(function () {
-//        
-//        
-//        
-//    });
-//}(jQuery));
+(function ($) {
+    $(function () {
+        
+        // Sorry for this. But I prefer it than editing phtml just for that. You may do it.
+        changeButtonsSetClass();
+        
+    });
+    
+    var changeButtonsSetClass = function () {
+        var _selector =
+            '#discount-coupon-form div.buttons-set'
+         +', #shipping-zip-form div.buttons-set'
+         +', #co-shipping-method-form div.buttons-set'
+         ;
+        
+        $(_selector).removeClass('buttons-set').addClass('buttons-set-box');
+    };
+    
+}(jQuery));
